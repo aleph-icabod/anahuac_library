c4995ba0649e6fc42499f8961cf196cce057d2d1
Miguel Bautista <angel_507@live.com.mx>
2023-03-08 10:18:39 -0600

    agregar imagenes

--- src/main/java/mx/anahuac/programacion_multimedia/Main.java
+++ src/main/java/mx/anahuac/programacion_multimedia/Main.java
@@ -1,9 +1,13 @@
 package mx.anahuac.programacion_multimedia;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
@@ -24,13 +28,19 @@ public class Main {
         panelOpciones.setLayout(new BoxLayout(panelOpciones,BoxLayout.Y_AXIS));
         panelOpciones.add(new JLabel("Selecciona una opcion"));
         JButton botonLibros=new JButton("Libros");
+
         JButton botonComics=new JButton("Comics");
         JButton botonManga=new JButton("Mangas");
         panelOpciones.add(botonLibros);
+        agregarImagen(panelOpciones);
+
+        agregarIconoBoton(botonLibros);
         panelOpciones.add(botonComics);
         panelOpciones.add(botonManga);
 
         panelOpciones.setBackground(Color.getHSBColor(100,50,20));
+
+
         ventana.add(panelOpciones);
 
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -63,4 +73,42 @@ public class Main {
 
         ventana.setVisible(true);
     }
+
+
+    public static void agregarImagen(JPanel contenedor){
+        try{
+            File file = new File("img.png");
+            BufferedImage bufferedImage = ImageIO.read(file);
+
+            ImageIcon icon= new ImageIcon(bufferedImage);
+            Image image = icon.getImage(); // transform it
+            Image newimg = image.getScaledInstance(100, 110,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+            icon = new ImageIcon(newimg);  // transform it back
+            JLabel label=new JLabel();
+            label.setIcon(icon);
+            contenedor.add(label);
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
+    public static void agregarIconoBoton(JButton button){
+        try{
+            File file = new File("img.png");
+            BufferedImage bufferedImage = ImageIO.read(file);
+
+            ImageIcon icon= new ImageIcon(bufferedImage);
+            Image image = icon.getImage(); // transform it
+            Image newimg = image.getScaledInstance(30, 30,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+            icon = new ImageIcon(newimg);  // transform it back
+
+            button.setIcon(icon);
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
 }
